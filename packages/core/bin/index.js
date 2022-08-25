5c9f495de975adbd45f72e9fb331d3a26f20cc7c
jinle <jinle0703@foxmail.com>
2022-08-25 19:33:29 +0800

    core link utils

--- packages/core/bin/index.js
+++ packages/core/bin/index.js
@@ -1,3 +1,4 @@
 #! /usr/bin/env node
 
+require('../lib/core')
 console.log('hello, jinle-cli')
--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -1,7 +1,13 @@
 'use strict';
 
+const utils = require('@jinle-cli/utils')
+
 module.exports = core;
 
 function core() {
     // TODO
+    utils()
+    console.log('core')
 }
+
+core()
